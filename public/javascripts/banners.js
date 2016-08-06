2ba7fbd4c51cb6d520a76671037d95821cdfd769
JamesMJordan <jamesmjordan00@gmail.com>
2016-08-05 17:12:53 -0700

    commitbitch

--- public/javascripts/banners.js
+++ public/javascripts/banners.js
@@ -1,25 +1,31 @@
-var banners = function (jQuery) {
+$(document).ready(function(){
+    const TAX = 1.08125;
+    const PRICE_SQFT = 2.75;
 
-    $("input").keyup(function() {
-        var heightIn = $("#heightin").val(),
-            widthIn = $("#widthin").val(),
-            heightFt = $("#heightft").val() + Math.round(heightIn + .5),
-            widthFt = $("#widthft").val() + Math.round(widthIn + .5),
-            squareFt = heightFt * widthFt,
-            tax = .08125,
-            pricesqft = 2.75,
-            qty = $("#qty").val();
+  function toInches(feet, inches){
+		return Math.round((parseInt(inches) / 12) + .42) + Math.round(parseInt(feet))
+	}
 
-        var newPrice = (squareFt * pricesqft) * qty * tax;
+  function calculatePrice(squareFeet, quantity){
+  	return ((((squareFeet * PRICE_SQFT) * quantity) * TAX) + 15);
+  }
 
+	$("input").keyup(function(){
+ 		var input = $(this).val();
+    if(input == ''){
+    	$(this).val('0');
+    }
 
+   	const heightIn = $("#heightin").val();
+   	const widthIn = $("#widthin").val();
+    const heightFt = $("#heightft").val();
+    const widthFt = $("#widthft").val();
+    const QUANTITY = parseInt($("#qty").val());
 
-            console.log(((squareFt * pricesqft) * qty) * tax);
-
-        $(".price").text(newPrice.toFixed(2));
-
-
-
-    });
-};
-$(document).ready(banners);
+    const SQUARE_FT = toInches(widthFt, widthIn) * toInches(heightFt, heightIn);
+		var newPrice = calculatePrice(SQUARE_FT, QUANTITY);
+    console.log(SQUAR_FT)
+    console.log(((SQUARE_FT * PRICE_SQFT) * QUANTITY) * TAX);
+   	$(".price").text(newPrice.toFixed(2));
+  });
+});
